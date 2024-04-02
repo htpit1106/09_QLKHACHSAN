dd28b6233db642e41f66fac737bef1e551d3aafd
htpit1106 <phuonggm204@gmail.com>
2024-04-02 19:55:53 +0700

    first commit

---
+++ 09_QLKHACHSAN/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace _09_QLKHACHSAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_tenkhach = new System.Windows.Forms.TextBox();
+            this.tb_sophongthue = new System.Windows.Forms.TextBox();
+            this.cb_loaiphong = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_timkiemten = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên khách hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Giới tính";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Loại phòng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số phòng thuê";
+            // 
+            // tb_tenkhach
+            // 
+            this.tb_tenkhach.Location = new System.Drawing.Point(105, 11);
+            this.tb_tenkhach.Name = "tb_tenkhach";
+            this.tb_tenkhach.Size = new System.Drawing.Size(186, 20);
+            this.tb_tenkhach.TabIndex = 4;
+            // 
+            // tb_sophongthue
+            // 
+            this.tb_sophongthue.Location = new System.Drawing.Point(105, 109);
+            this.tb_sophongthue.Name = "tb_sophongthue";
+            this.tb_sophongthue.Size = new System.Drawing.Size(186, 20);
+            this.tb_sophongthue.TabIndex = 5;
+            // 
+            // cb_loaiphong
+            // 
+            this.cb_loaiphong.FormattingEnabled = true;
+            this.cb_loaiphong.Location = new System.Drawing.Point(105, 78);
+            this.cb_loaiphong.Name = "cb_loaiphong";
+            this.cb_loaiphong.Size = new System.Drawing.Size(121, 21);
+            this.cb_loaiphong.TabIndex = 6;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(105, 45);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Nam";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(206, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Nu";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Tên";
+            // 
+            // tb_timkiemten
+            // 
+            this.tb_timkiemten.Location = new System.Drawing.Point(63, 15);
+            this.tb_timkiemten.Name = "tb_timkiemten";
+            this.tb_timkiemten.Size = new System.Drawing.Size(186, 20);
+            this.tb_timkiemten.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Tìm kiếm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Thêm";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(123, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(215, 104);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Sửa";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 180);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(606, 259);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 11);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(609, 163);
+            this.tableLayoutPanel1.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.tb_timkiemten);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(307, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(299, 157);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tb_tenkhach);
+            this.panel1.Controls.Add(this.tb_sophongthue);
+            this.panel1.Controls.Add(this.cb_loaiphong);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(298, 157);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 442);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quan ly khách sạn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_tenkhach;
+        private System.Windows.Forms.TextBox tb_sophongthue;
+        private System.Windows.Forms.ComboBox cb_loaiphong;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_timkiemten;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ 09_QLKHACHSAN/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09_QLKHACHSAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        string chuoiketnoi = "Data Source=DESKTOP-GE1C4PK\\SQLEXPRESS;Initial Catalog=QLKhachSan;Integrated Security=True";
+        
+        void loadData()
+        {
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from khachhang";
+            adapter.SelectCommand = cmd;
+            dt.Clear();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+        void runcommand(string query)
+        {
+            cmd = conn.CreateCommand();
+            cmd.CommandText= query;
+            cmd.ExecuteNonQuery();
+            loadData();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(chuoiketnoi);
+            conn.Open();
+            loadData();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string gioitinh;
+            if (radioButton1.Checked)
+            {
+                gioitinh = "Nam";
+            }
+            else gioitinh = "Nu";
+
+            string query = "insert into khachhang ([Họ tên] ,[Giới tính],[Loại phòng],[Số phòng thuê]  ) " +
+                "values (@hoten, @gioitinh, @loaiphong, @sophongthue)";
+            cmd = conn.CreateCommand();
+            cmd.Parameters.Add("@hoten", tb_tenkhach.Text);
+            cmd.Parameters.Add("@gioitinh", gioitinh);
+            cmd.Parameters.Add("@loaiphong", cb_loaiphong.Text);
+            cmd.Parameters.Add("@sophongthue", Convert.ToInt32(tb_sophongthue.Text));
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            loadData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            
+                int ma = Convert.ToInt32( dataGridView1.Rows[i].Cells[0].Value);
+                cmd = conn.CreateCommand();
+
+                string query = "delete from khachhang where [Mã KH] =  @ma";
+                cmd.Parameters.Add("@ma", ma);
+
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+                loadData();
+            
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.CurrentRow;
+            tb_tenkhach.Text = row.Cells[1].Value.ToString();
+            string gioitinh = row.Cells[2].Value.ToString();
+            if (gioitinh == "Nam") radioButton1.Checked = true;
+            else radioButton2.Checked = true;
+            cb_loaiphong.Text = row.Cells[3].Value.ToString();
+            tb_sophongthue.Text =row.Cells[4].Value.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            string gioitinh;
+            if (radioButton1.Checked)
+            {
+                gioitinh = "Nam";
+            }
+            else gioitinh = "Nu";
+            int i = dataGridView1.CurrentRow.Index;
+
+            int ma = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+            cmd = conn.CreateCommand();
+            string query = "update khachhang set [Họ tên] = @hoten , [Giới tính] = @gioitinh, [Loại phòng] = @loaiphong , [Số phòng thuê]  =  @sophongthue  where [Mã KH] =  @ma";
+            cmd.Parameters.Add("@hoten", tb_tenkhach.Text);
+            cmd.Parameters.Add("@gioitinh", gioitinh);
+            cmd.Parameters.Add("@loaiphong",  cb_loaiphong.Text);
+            cmd.Parameters.Add("@sophongthue",Convert.ToInt32(tb_sophongthue.Text));
+            cmd.Parameters.Add("@ma", ma);
+
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            loadData();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmd = conn.CreateCommand();
+            string query = "select * from khachhang where [Họ tên] = @hoten";
+            cmd.Parameters.AddWithValue("@hoten", tb_timkiemten.Text);
+            cmd.CommandText = query;
+            adapter.SelectCommand = cmd;
+            dt.Clear();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
